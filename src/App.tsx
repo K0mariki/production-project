36ce3bd8fb4d9dc3a8e110c6357dff462339d78d
Ермолов Андрей <a.ermolov@worksolutions.ru>
2026-05-26 17:22:17 +0300

    Добавлены базовые стили + светлая/тёмная тема

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import "./index.scss"
+import "./styles/index.scss"
 import {Routes, Route, Link} from "react-router-dom";
-import {Suspense} from "react";
+import {Suspense, useContext} from "react";
 import {MainPageAsync} from "./pages/MainPage/MainPageAsync";
 import {AboutPageAsync} from "./pages/AboutPage/AboutPageAsync";
-import { Counter } from "./components/Counter";
+import {Theme, ThemeContext} from "./theme/ThemeContext";
+import {useTheme} from "./theme/useTheme";
 
 function App(){
+  const {theme, toggleTheme} = useTheme()
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>{theme === Theme.LIGHT ? Theme.DARK : Theme.LIGHT}</button>
       <Link to="/">Home</Link>
       <Link to="/about">About</Link>
       <Suspense fallback={<div>Loading...</div>}>
@@ -16,7 +20,6 @@ function App(){
           <Route path="/about" element={<AboutPageAsync />} />
         </Routes>
       </Suspense>
-      <Counter />
     </div>
   )
 }
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,5 +1,8 @@
 export default function MainPage() {
   return (
-    <div>MainPage</div>
+    <div>
+      <h1>Заголовк 1</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, architecto aspernatur corporis cumque ipsam molestiae nam nobis obcaecati placeat, porro possimus quasi qui, saepe totam vel vitae voluptates voluptatibus! Necessitatibus!</p>
+    </div>
   );
 }
