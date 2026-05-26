94af19786e329bb9b169f94052b219cce82a1ead
Ермолов Андрей <a.ermolov@worksolutions.ru>
2026-05-26 11:28:47 +0300

    Добавлен react-router-dom и тестовая нацигация

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-import { Counter } from "./components/Counter";
 import "./index.scss"
+import {Routes, Route, Link} from "react-router-dom";
+import {Suspense} from "react";
+import {MainPageAsync} from "./pages/MainPage/MainPageAsync";
+import {AboutPageAsync} from "./pages/AboutPage/AboutPageAsync";
+import { Counter } from "./components/Counter";
 
 function App(){
   return (
     <div className="app">
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<MainPageAsync />} />
+          <Route path="/about" element={<AboutPageAsync />} />
+        </Routes>
+      </Suspense>
       <Counter />
     </div>
   )
---
+++ src/pages/AboutPage/AboutPageAsync.tsx
@@ -0,0 +1,7 @@
+import {lazy} from "react";
+
+export const AboutPageAsync = lazy(() => new Promise(resolve =>
+  // @ts-ignore
+  // Имитация запроса
+  setTimeout(() => resolve(import("./AboutPage")), 1500))
+);
